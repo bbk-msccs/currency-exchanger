c3345607bfa1ad2cdcaf44b01a2e778c581e59f2
bbk-pij-2016-44 <nmonta01@dcs.bbk.ac.uk>
2016-10-10 19:38:57 +0100

    42

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,3 +1,6 @@
+println("Hello, Nick")
+
+
 print "If I give you £1, how many Euro will you give me? ";
 double euroOverPoundRatio = Double.parseDouble(System.console().readLine());
 print "If I give you 1€, how many Sterling pounds will you give me? ";
